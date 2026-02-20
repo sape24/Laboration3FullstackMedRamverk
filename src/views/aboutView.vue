a27d217c46715a944910082d44ad07390141aeec
Samuel Petersson <samuel.petersson@hotmail.com>
2026-02-20 15:16:52 +0100

    Färdig infosida

--- src/views/aboutView.vue
+++ src/views/aboutView.vue
@@ -4,8 +4,11 @@ import articleComponent from '@/components/articleComponent.vue';
 
 <template>
     <articleComponent
-        title="om test"
-        text="om test"
+        title="Mina tankar om VUE.js"
+        text="Jag installerade viue.js med hjälp av Vite genom kommandot: npm create vue@latest. Det var att smidigt sätt att snabbt få upp en projektstruktur med mappar och configs klara.
+        Vi har arbetat med komponeneter innan men precis som då kändes det logiskt. När jag väl förstod hur props och emits fungerar blev det enkelt att flytta data mellan tabellen och huvudsidan. Även v-model gjorde det väldigt
+        tacksamt att arbeta med formulär. Det svåraste var definitivt att förstå sig på just emits och props, vi har aldrig arbetat med det innan vilket gjorde att det kändes lite komplext till en början. Vue känns som ett väldigt
+        kraftfullt men ändå lättföråeligt ramverk. Jag gillar balansen mellan enkel HTML och reaktiv javascript, att utforska vue mer i framtiden är något jag definitivt kommer att göra."
     />
 </template>
 
